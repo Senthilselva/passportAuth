6a25fb8445c063777832137c7bc3dcbd5f0be9c0
Senthilselva <senthilselvak@gmail.com>
2017-01-02 12:39:18 -0500

    building skeleton

---
+++ server.js
@@ -0,0 +1,75 @@
+//Dependencies
+// ============
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser'); // for working with cookies
+var bodyParser = require('body-parser');
+var session = require('express-session'); 
+var methodOverride = require('method-override'); // for deletes in express
+var debug = require('debug')('express-example');
+
+// Our model controllers (rather than routes)
+//var application_controller = require('./controllers/applicationControllers');
+//var employee_controller = require('./controllers/employeeControllers');
+
+// instantiate our app
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+
+// override POST to have DELETE and PUT
+app.use(methodOverride('_method'))
+
+//allow sessions
+app.use(session({ secret: 'app', cookie: { maxAge: 60000 }}));
+app.use(cookieParser());
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/assets/img/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//app.use('/', application_controller);
+//app.use('/employee', employee_controller);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// no stacktraces leaked to user unless in development environment
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: (app.get('env') === 'development') ? err : {}
+  })
+});
+
+
+
+// we bring in the models we exported with index.js
+var models = require("./models");
+// we set the port of the app
+app.set('port', process.env.PORT || 3000);
+
+// we sync the models with our db 
+// (thus creating the apropos tables)
+models.sequelize.sync().then(function () {
+	// set our app to listen to the port we set above
+  var server = app.listen(app.get('port'), function() {
+  	// then save a log of the listening to our debugger.
+    console.log('Express server listening on port ' + server.address().port);
+  });
+});
